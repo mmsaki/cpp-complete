12bc7f1092b71ef8daffc0482bdd5de3d888da40
meeky <98189596+mmsaki@users.noreply.github.com>
2023-04-25 02:05:10 -0500

    section 3 comment

---
+++ 03-comments/.vscode/main.cpp
@@ -0,0 +1,18 @@
+// one line comment
+
+// this bring in the iostream library
+#include <iostream>
+
+/*
+  This is a comment block
+  that spans multiple lines.
+  You cannot nest your comments
+*/
+
+int main()
+{
+  // this prints out hello world
+  std::cout << "Hello World";
+  return 0;
+  // this program ends here
+}
